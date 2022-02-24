7999df0c450e88ea1a4d71569fcf844a12b283ec
Khasan-Meliev <khasanmeliev2@gmail.com>
2022-02-24 20:11:36 +0500

    can_you_write

---
+++ script.js
@@ -0,0 +1,42 @@
+function openBox() {
+  document.getElementById("button1").style.display = "none";
+  document.getElementById("box").style.display = "flex";
+}
+const words = [
+  "dog",
+  "cat",
+  "horse",
+  "puppy",
+  "kitten",
+  "kangaroo",
+  "cow",
+  "horse",
+  "puppy",
+  "kitten",
+  "kangaroo",
+  "monkey",
+  "lion",
+  "tiger",
+  "wolf",
+  "fox",
+  "rabbit",
+  "camel",
+  "donkey",
+];
+const word = document.getElementById("randomWord");
+const lastCharacter = document.getElementById("lastWord");
+function random() {
+  const randomWord = Math.floor(Math.random() * words.length);
+  const output = words[randomWord];
+  console.log(output);
+  word.value = output;
+}
+function submit() {
+  if (lastCharacter.value == word.value) {
+    alert(`you can write ${word.value} text`);
+    word.value = "";
+    lastCharacter.value = "";
+  } else {
+    alert("false");
+  }
+}
